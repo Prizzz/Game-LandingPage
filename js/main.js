b5ad82c1626da637fa7c30b29dbe84185e3b58e4
Prizzz <38956893+Prizzz@users.noreply.github.com>
2020-12-28 20:12:39 +0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,31 @@
+$('img.img-svg').each(function(){
+    var $img = $(this);
+    var imgClass = $img.attr('class');
+    var imgURL = $img.attr('src');
+    $.get(imgURL, function(data) {
+    var $svg = $(data).find('svg');
+    if(typeof imgClass !== 'undefined') {
+        $svg = $svg.attr('class', imgClass+' replaced-svg');
+    }
+     $svg = $svg.removeAttr('xmlns:a');
+    if(!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
+        $svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'))
+    }
+    $img.replaceWith($svg);
+    }, 'xml');
+    });
+
+
+$(".navbar__lang-switch-arrow").click(function(){
+    $(".navbar__lang-switch-hidden").toggleClass("is-open")
+})
+
+$('.menu-burger').click(function(){
+    $(".hidden-menu").animate({'right':'0'}, 200);
+    $(this).hide()
+})
+
+$('.close').click(function(){
+    $(".hidden-menu").animate({'right':'-80%'}, 200);
+    $('.menu-burger').show()
+})
